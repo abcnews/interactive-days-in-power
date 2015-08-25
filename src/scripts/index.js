d124918b28bc6cf464f6424054c83c765fdaa086
Simon Elvery <elvery.simon@abc.net.au>
2015-08-25 16:00:51 +1000

    1.0-ish

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -21,20 +21,17 @@ var checkExist = setInterval(function() {
 function init() {
 	var container, svg, margin, x, y, xAxis, yAxis, width, height, line, dataUrl, series;
 
-	// d3 labeler extenstion
-	require('./labeler');
-
 	container = document.getElementById('interactive-captains-knock-charts');
 
 	dataUrl = document.location.host.indexOf(':8000') === -1 ?
-		'/res/sites/news-projects/interactive-captains-knock-charts/data/cumulative-runs.csv' :
+		ABC.News.utilities.getResHost() + '/res/sites/news-projects/interactive-captains-knock-charts/1.0.0/data/cumulative-runs.csv' :
 		'/data/cumulative-runs.csv';
 
 	margin = {
-		top: 20,
-		right: 20,
+		top: 10,
+		right: 25,
 		bottom: 20,
-		left: 50
+		left: 12
 	};
 
 	width = $(container).innerWidth() - margin.left - margin.right;
@@ -52,7 +49,7 @@ function init() {
 
 	yAxis = d3.svg.axis()
 		.scale(y)
-		.orient('left');
+		.orient('right');
 
 	line = d3.svg.line()
 		.x(function(d) {return x(d.inning);})
@@ -66,14 +63,14 @@ function init() {
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	d3.csv(dataUrl, function(err, data){
-		var players;
+		var players, labels;
 
 		players = {};
 
 		data.forEach(function(d) {
 			var key;
 			for (key in d) {
-				if (key !== 'inning' && d[key] !== '') {
+				if (key !== 'inning' && d[key] !== '' && typeof d[key] !== 'undefined') {
 					players[key] = players[key] || [{inning:0, runs: 0}];
 					players[key].push({inning: +d['inning']+1, runs: +d[key]});
 				}
@@ -82,8 +79,15 @@ function init() {
 
 		series = d3.entries(players);
 
+		series.forEach(function(d){
+			d.totalRuns = d3.max(d.value, function(d){return d.runs;});
+			d.totalInnings = d3.max(d.value, function(d){return d.inning;});
+		});
+
 		x.domain([0,d3.max(series, function(d){return d3.max(d.value, function(d){return d.inning;});})]);
 		y.domain([d3.max(series, function(d){return d3.max(d.value, function(d){return d.runs;});}),0]);
+
+		yAxis.tickSize(width, 0);
 
 		svg.append('g')
 			.attr('class', 'x axis')
@@ -101,10 +105,13 @@ function init() {
 			.call(yAxis)
 			.append('text')
 				.attr('transform', 'rotate(-90)')
-				.attr('y', 6)
+				.attr('y', -12)
 				.attr("dy", ".71em")
 				.style("text-anchor", "end")
 				.text("Cumulative runs");
+
+
+		svg.selectAll('.y.axis .tick text').attr('transform', 'translate('+ -width + ',-10)');
 
 		svg.selectAll('.line').data(series, lineKey)
 			.enter().append('g').attr('class', function(d){
@@ -113,19 +120,60 @@ function init() {
 				.datum(function(d){return d.value;})
 				.attr("d", line);
 
-		// var labels = d3.labeler()
-		// 	.label(series.map(function(d){
-		// 		return {
-		// 			x: d3.max(d.value.runs),
-		// 			y: d3.max(d.value.inning),
-		// 		}
-		// 	}))
-		// 	.anchor(anchor_array)
-		// 	.width(width)
-		// 	.height(height)
-		// 	.start(nsweeps);
+		svg.selectAll('.line').each(function(d){
+			if (d.key === 'Michael Clarke') {
 
+				this.parentNode.appendChild(this);
+			}
+		});
+
+		// Draw labels
+		labels = svg.selectAll(".line-label").data(series, lineKey)
+			.enter().append("text")
+				.attr("class", function(d){
+					return 'line-label ' + slugify(d.key);
+				})
+				.attr('text-anchor', function(d) {
+					if (d.key === 'Ricky Ponting') {
+						return 'end';
+					}
+
+					return 'middle';
+				})
+				.text(function(d) { return d.key; })
+				.attr("x", labelX)
+				.attr("y", labelY)
+				.attr("fill", "black");
+
+		labels.each(function(d) {
+			d.labelSize = [this.getBBox().width,this.getBBox().height];
+		});
 	});
+
+	function labelX(d) {
+		var natural = x(d.totalInnings);
+		if (d.key === 'Ricky Ponting') {
+			return natural - 10;
+		}
+		return natural;
+	}
+
+	function labelY(d) {
+		var natural = y(d.totalRuns)-5;
+		if (d.key === 'Ricky Ponting') {
+			return natural + 5;
+		}
+		if (d.key === 'Donald Bradman') {
+			return natural - 5;
+		}
+		if (d.key === 'Ian Chappell') {
+			return natural - 5;
+		}
+		if (d.key === 'Allan Border') {
+			return natural - 5;
+		}
+		return natural;
+	}
 
 	function lineKey(d) {
 		return d.key;
@@ -140,6 +188,7 @@ function init() {
 
 		x.range([0, width]);
 		y.range([0, height]);
+		yAxis.tickSize(width, 0);
 
 		d3.select('.x.axis')
 			.attr('transform', 'translate(0, '+height +')')
@@ -154,6 +203,8 @@ function init() {
 			.select('.chart')
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+		svg.selectAll('.y.axis .tick text').attr('transform', 'translate('+ -width + ',-10)');
+
 		update = svg.selectAll('.line').data(series, lineKey);
 		update.select('path')
 			.datum(function(d){return d.value;})
@@ -161,6 +212,11 @@ function init() {
 		update.exit().remove();
 
 		d3.select('.x.axis .label').attr('x', width);
+
+		svg.selectAll(".line-label")
+			.attr("x", labelX)
+			.attr("y", labelY)
+			.attr("fill", "black");
 	});
 
 }
